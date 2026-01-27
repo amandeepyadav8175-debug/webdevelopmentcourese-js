9d4f998f075bac62e09f8d58fc51eb06ddf5813b
amandeepyadav8175-debug <amandeepyadav8175@gmail.com>
2026-01-27 12:48:41 +0000

    code completed

--- next.js
+++ next.js
@@ -1,1 +1,21 @@
+const accountId= 122344
+let accountEmail= "abcds@gmail.com";
+var accountPassword="1233455";
+accountcity= "mumbai";
+let name;
+var add
+//accountId= 2344555 not allowed
+ accountEmail= "abceeeeds@gmail.com";
+ accountPassword="12789007633455";
+accountcity= "mumbaiwala";
+
+/*
+dont use var beacuse of  block scope and functional scope
+
+*/
+
+
+
+
+console.table([accountId,accountEmail,accountPassword,accountcity,name,add])
 
